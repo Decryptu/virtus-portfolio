1a3082bfadcc96f5005c880564d131d73265b491
Decrypt <louis.vicomte@protonmail.com>
2024-05-09 15:51:29 +0200

    google verif

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,7 @@ export default function RootLayout({
     >
       <head>
         <SandpackCSS />
+        <meta name="google-site-verification" content="xq88omFRK6Z-xLu0fnJce6EKll5kmZt9SffRLdNBYuk" />
       </head>
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
